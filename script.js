c87b5d74caf8cae2d8187c69762f6f61d3c906f3
Yazılım Kafası <79590484+yazilimkafasi@users.noreply.github.com>
2021-10-12 14:23:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "70%"
+    document.getElementById('backdrop').style.display = "block"
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0"
+    document.getElementById('backdrop').style.display = "none"
+}
